58a8a12ffb111384f96f13fd19ed0526631e9965
Andrew Scott <adscott1982@googlemail.com>
2017-11-26 21:12:08 +0000

    Refactored movement code into methods.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -75,39 +75,18 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateMovement()
     {
-        // Clamp max lateral speed
-        if (Math.Abs(this.rb.velocity.x) > this.MaxSpeed)
-        {
-            var velocity = this.rb.velocity;
-            velocity.x = velocity.x > 0 ? this.MaxSpeed : -this.MaxSpeed;
-            this.rb.velocity = velocity;
-        }
+        this.ClampMaxLateralSpeed();
 
         var leftThumbstick = InputManager.GetLeftThumbstick();
 
         // If the player has released the lateral movement, and the player is moving laterally, decelerate
         if (Math.Abs(leftThumbstick.x) < Tools.FloatEqualityTolerance && Math.Abs(this.rb.velocity.x) > Tools.FloatEqualityTolerance)
         {
-            var velocity = this.rb.velocity;
-            var deceleration = this.isGrounded ? this.IdleDecelerationGrounded * Time.fixedDeltaTime :
-                                                 this.IdleDecelerationAir * Time.fixedDeltaTime;
-
-            if (Mathf.Abs(velocity.x) - deceleration < 0)
-            {
-                velocity.x = 0f;
-            }
-            else
-            {
-                velocity.x = velocity.x > 0 ? velocity.x - deceleration : velocity.x + deceleration;
-            }
-            
-            this.rb.velocity = velocity;
+            this.DecelerateLateralSpeed();
         }
         else // Else apply lateral movement
         {
-            var lateralForce = this.isGrounded ? this.GroundAccelerationForce : this.AirAccelerationForce;
-            var lateralForceVector = new Vector2(leftThumbstick.x * lateralForce, 0);
-            this.rb.AddForce(lateralForceVector);
+            this.ApplyLateralForce(leftThumbstick.x);
         }
 
         // If on a wall, and the left thumbstick is pressed in a direction, clamp vertical falling speed to wall slide speed
@@ -116,10 +95,7 @@ public class PlayerMovement : MonoBehaviour
             // If wall fall speed is greater than the defined speed
             if (this.rb.velocity.y < -this.WallSlideSpeed)
             {
-                var velocity = this.rb.velocity;
-                velocity.y += this.WallSlideDeceleration;
-                velocity.y = velocity.y > -this.WallSlideSpeed ? -this.WallSlideSpeed : velocity.y;
-                this.rb.velocity = velocity;
+                this.ControlWallSlideVerticalSpeed();
             }
         }
 
@@ -131,19 +107,77 @@ public class PlayerMovement : MonoBehaviour
             }
             else if (this.isTouchingLeftWall || this.isTouchingRightWall) // Else if on a wall, wall jump
             {
-                // If falling stop vertical velocity
-                if (this.rb.velocity.y < 0)
-                {
-                    var velocity = this.rb.velocity;
-                    velocity.y = 0;
-                    this.rb.velocity = velocity;
-                }
-
-                var jumpDirection = this.isTouchingLeftWall ? new Vector2(1f, 1.2f) : new Vector2(-1f, 1.2f);
-                jumpDirection = jumpDirection.normalized;
-                this.rb.AddForce(jumpDirection * this.JumpForce);
+                this.DoWallJump();
             }
         }
+    }
+
+    private void ClampMaxLateralSpeed()
+    {
+        // If not currently at max speed, return
+        if (!(Math.Abs(this.rb.velocity.x) > this.MaxSpeed))
+        {
+            return;
+        }
+
+        var velocity = this.rb.velocity;
+        velocity.x = velocity.x > 0 ? this.MaxSpeed : -this.MaxSpeed;
+        this.rb.velocity = velocity;
+    }
+
+    private void DecelerateLateralSpeed()
+    {
+        var velocity = this.rb.velocity;
+        var deceleration = this.isGrounded ? this.IdleDecelerationGrounded * Time.fixedDeltaTime :
+            this.IdleDecelerationAir * Time.fixedDeltaTime;
+
+        if (Mathf.Abs(velocity.x) - deceleration < 0)
+        {
+            velocity.x = 0f;
+        }
+        else
+        {
+            velocity.x = velocity.x > 0 ? velocity.x - deceleration : velocity.x + deceleration;
+        }
+
+        this.rb.velocity = velocity;
+    }
+
+    private void ApplyLateralForce(float fraction)
+    {
+        var lateralForce = this.isGrounded ? this.GroundAccelerationForce : this.AirAccelerationForce;
+        var lateralForceVector = new Vector2(fraction * lateralForce, 0);
+        this.rb.AddForce(lateralForceVector);
+    }
+
+    private void DoWallJump()
+    {
+        // If falling stop vertical velocity
+        if (this.rb.velocity.y < 0)
+        {
+            var velocity = this.rb.velocity;
+            velocity.y = 0;
+            this.rb.velocity = velocity;
+        }
+
+        var jumpDirection = this.isTouchingLeftWall ? new Vector2(1f, 1.2f) : new Vector2(-1f, 1.2f);
+        jumpDirection = jumpDirection.normalized;
+        this.rb.AddForce(jumpDirection * this.JumpForce);
+    }
+
+    private void ControlWallSlideVerticalSpeed()
+    {
+        if (!(this.rb.velocity.y < -this.WallSlideSpeed))
+        {
+            return;
+        }
+
+        // If wall fall speed is greater than the max slide speed
+
+        var velocity = this.rb.velocity;
+        velocity.y += this.WallSlideDeceleration;
+        velocity.y = velocity.y > -this.WallSlideSpeed ? -this.WallSlideSpeed : velocity.y;
+        this.rb.velocity = velocity;
     }
 
     private void OnCollisionStay2D(Collision2D other)
