4a488268da9feb46da8d2398bf42b96049646af3
Andrew Scott <adscott1982@googlemail.com>
2018-07-16 22:11:15 +0100

    Commit prior to creating new scene.

---
+++ Assets/PlayerExit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerExit : MonoBehaviour
+{
+    private bool playerCollided;
+    private Rigidbody2D playerRb;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+		if (!this.playerCollided)
+        {
+            return;
+        }
+
+        // Player has collided
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            this.playerCollided = true;
+        }
+    }
+}
